0e78a0162668ba57e5474790ee26466a915760fd
kagemeka <kagemeka1@gmail.com>
2022-02-15 21:35:09 +0900

    env: update

--- scripts/generate_sphinx_docs_headers.py
+++ scripts/generate_sphinx_docs_headers.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append(".")
 from scripts._docs_util import ROOT, get_sphinx_docs_directory
 
-SPHINX_DOCS_HEADER = "Python\ Project\ Template"
+SPHINX_DOCS_HEADER = "Algo Method"
 
 
 def _remove_current_docs(docs_path: str) -> None:
